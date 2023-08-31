e0778fe705e73d2ef98782e557e69f35f1074336
Xinhao Li <47157848+XinhaoLi24@users.noreply.github.com>
2023-08-31 14:53:50 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../surface_pd'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 import mock
 MOCK_MODULES = ['numpy', 'matplotlib', 'pandas', 'pymatgen', 'monty', 'surface_pd']
